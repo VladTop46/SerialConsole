34cf34b3086d024874df27cd25bfc922561faa3e
VladTop46 <mcsm2654@gmail.com>
2023-12-23 13:55:26 +0900

    Add files

---
+++ SerialConsole/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SerialConsole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxReceived = new System.Windows.Forms.RichTextBox();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBaudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxReceived
+            // 
+            this.textBoxReceived.Location = new System.Drawing.Point(12, 12);
+            this.textBoxReceived.Name = "textBoxReceived";
+            this.textBoxReceived.ReadOnly = true;
+            this.textBoxReceived.Size = new System.Drawing.Size(434, 415);
+            this.textBoxReceived.TabIndex = 0;
+            this.textBoxReceived.Text = "";
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(53, 433);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(393, 20);
+            this.textBoxSend.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(680, 415);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(108, 38);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 440);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ввод:";
+            // 
+            // cmbBaudRate
+            // 
+            this.cmbBaudRate.FormattingEnabled = true;
+            this.cmbBaudRate.Location = new System.Drawing.Point(548, 12);
+            this.cmbBaudRate.Name = "cmbBaudRate";
+            this.cmbBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cmbBaudRate.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(452, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Скорость порта:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(482, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "COM порт:";
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(548, 51);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbPorts.TabIndex = 7;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(713, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(713, 41);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 465);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cmbPorts);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBaudRate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.textBoxReceived);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SerialConsole";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBoxReceived;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBaudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ SerialConsole/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace SerialConsole
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+        }
+
+        private void InitializeSerialPort()
+        {
+            serialPort = new SerialPort();
+            serialPort.DataReceived += SerialPort_DataReceived;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort.PortName = cmbPorts.SelectedItem.ToString();
+                serialPort.BaudRate = (int)cmbBaudRate.SelectedItem;
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при открытии порта: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при закрытии порта: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+             string receivedData = serialPort.ReadExisting();
+            this.Invoke((MethodInvoker)delegate { textBoxReceived.Text += receivedData; });
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            cmbPorts.DataSource = SerialPort.GetPortNames();
+            cmbBaudRate.DataSource = new[] { 9600, 19200, 38400, 57600, 115200 };
+            cmbPorts.SelectedIndex = 0;
+            cmbBaudRate.SelectedIndex = 0;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string dataToSend = textBoxSend.Text;
+            SendData(dataToSend);
+        }
+
+        private void SendData(string data)
+        {
+            if (serialPort.IsOpen && !string.IsNullOrEmpty(data))
+            {
+                try
+                {
+                    serialPort.Write(data);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при отправке данных: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Порт не открыт или данные пусты.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
